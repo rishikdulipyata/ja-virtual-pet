5cdc5172201b8ebb859b2ce2e50b4e4b03200534
Zachary Kramer <zckramer83@gmail.com>
2023-03-24 15:50:48 -0400

    add PetNeeds class. Start a new pet menu.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int select;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Greetings, young one... welcome to the world. By what name shall your friends call you?");
+        String petName = input.nextLine();
+
+        PetNeeds pet = new PetNeeds(petName);
+        
+        birthMenu(pet);
+    }
+
+    public static void birthMenu (PetNeeds newPet) {
+        System.out.println(
+            "It's a pleasure to meet you, " + newPet.getName() + "! I hope you will care for yourself during your time in this place...\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n" 
+        );
+
     }
 }
